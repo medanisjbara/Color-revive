e33447e9cf88b014a3b15bd0710073eb61a4aa01
jbara <medanisjbara@proton.me>
2023-12-18 23:49:42 +0100

    refactor: code structure improvement

--- colrev.py
+++ colrev.py
@@ -2,6 +2,8 @@
 from PIL import Image
 from sys import argv
 
+CONTRAST_OVERFLOW = 1
+
 def help():
 	print(f'usage: {argv[0]} <file>')
 	exit()
@@ -10,13 +12,13 @@ def rescale(t,min,max):
 	assert len(t) == len(min) == len(max)
 	out = []
 	for i in range(len(t)):
-		out.append (int( ( (t[i]-min[i]) / (max[i]-min[i]) ) * 255 * Contrast_overflow) )
+		out.append (int( ( (t[i]-min[i]) / (max[i]-min[i]) ) * 255 * CONTRAST_OVERFLOW) )
 	if len(t)==4:
 		out[-1]=255
 	out =  tuple(out)
 	return out
 
-def get_borders(input):
+def get_min_max_values(image_pixels):
 	all_input  = []
 	for x in range(width):
 		for y in range(height):
@@ -25,23 +27,29 @@ def get_borders(input):
 	max_input  = max(all_input)
 	return min_input, max_input
 
-if __name__=='__main__':	
-	input_file = argv[1] if len(argv) > 1 else ''
-	if not input_file: raise IOError ('No input file: please use command line arguments')
-	if input_file=='-h': help()
 
-	image  = Image.open(input_file)
+def process_image(file):
+	image  = Image.open(file) # TODO: File error handling
 	image_pixels  = image.load()
 	width, height = image.size
 
-	Contrast_overflow = 1
-
-	min_input, max_input = get_borders(image)
+	min_input, max_input = get_min_max_values(image_pixels)
 
 	for x in range(width):
 		for y in range(height):
 			pixel = image_pixels[x,y]
 			pixel = rescale (pixel, min_input, max_input)
 			image_pixels[x,y] = pixel
-	output_file = input_file.replace('.','_adjusted.') if '.' in input_file else input_file+'_adjusted'
+	output_file = file.replace('.','_adjusted.') if '.' in file else file+'_adjusted'
 	image.save(output_file)
+
+if __name__=='__main__':
+    # TODO: parse arguments with argparse
+	input_file = argv[1] if len(argv) > 1 else ''
+	if not input_file:
+        raise IOError ('No input file: please use command line arguments')
+	if input_file=='-h':
+        help()
+
+    # TODO: check for file extension
+    process_image(input_file)
